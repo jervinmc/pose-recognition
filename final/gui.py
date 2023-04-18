98a7d2b3adb072a7ca8ca992ead3ad6ae3f3212d
Jervin Macalawa <jervinmac@yahoo.com>
2023-04-18 20:42:44 +0800

    done

--- final/gui.py
+++ final/gui.py
@@ -3,14 +3,54 @@ import cv2
 import mediapipe as mp
 import threading
 import speech_recognition as sr
+# import webview
+
 r = sr.Recognizer()
 mic = sr.Microphone()
 mp_pose = mp.solutions.pose
 
 detected = ''
+category = ''
+speech = ''
+
+def create_activity_gui():
+    # create a new Tkinter window
+    activity_window = tk.Toplevel()
+
+    # set the window title
+    activity_window.title("Choose an Activity")
+
+    # create a label to describe the available activities
+    activity_label = tk.Label(activity_window, text="Please select an activity:")
+    activity_label.pack()
+
+    # create buttons for each activity
+    standing_button = tk.Button(activity_window,command=lambda : choose_category('standing still') ,text="Standing Still")
+    clapping_button = tk.Button(activity_window,command=lambda : choose_category('clapping'), text="Clapping")
+    sleeping_button = tk.Button(activity_window,command=lambda : choose_category('sleeping'), text="Sleeping")
+    stretching_button = tk.Button(activity_window, text="Stretching")
+    raising_button = tk.Button(activity_window,command=lambda : choose_category('raising arms'), text="Raising Arms")
+    pointing_button = tk.Button(activity_window,command=lambda : choose_category('pointing'), text="Pointing")
+
+    # add the buttons to the window
+    standing_button.pack()
+    clapping_button.pack()
+    sleeping_button.pack()
+    stretching_button.pack()
+    raising_button.pack()
+    pointing_button.pack()
+
+
+def choose_category(cat):
+    global category
+    category = cat
+    start_detection()
 
 def worker(num):
+    global speech
+    global category
     global detected
+    global speech
     while True:
         with mic as source:
             r.adjust_for_ambient_noise(source)
@@ -19,7 +59,9 @@ def worker(num):
             text = r.recognize_google(audio)
             print(f'detected {detected}')
             print(f"You said: ", text)
-            if(detected==text):
+            speech = text
+            if(detected==text and category==detected):
+                
                 print('Correct!')
             else:
                 print('Wrong!')
@@ -32,9 +74,12 @@ def worker(num):
 
 def start_detection():
     global detected
+    global speech
+    score = 0
     t = threading.Thread(target=worker, args=('',))
     t.start()
     mp_pose = mp.solutions.pose
+    standing_still_keypoints = [mp.solutions.pose.PoseLandmark.LEFT_SHOULDER, mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER,mp.solutions.pose.PoseLandmark.LEFT_HIP, mp.solutions.pose.PoseLandmark.RIGHT_HIP,mp.solutions.pose.PoseLandmark.LEFT_KNEE, mp.solutions.pose.PoseLandmark.RIGHT_KNEE,mp.solutions.pose.PoseLandmark.LEFT_ANKLE, mp.solutions.pose.PoseLandmark.RIGHT_ANKLE]
     clapping_keypoints = [mp.solutions.pose.PoseLandmark.LEFT_WRIST, mp.solutions.pose.PoseLandmark.RIGHT_WRIST]
     hi_keypoints = [mp.solutions.pose.PoseLandmark.LEFT_SHOULDER, mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER, 
                     mp.solutions.pose.PoseLandmark.LEFT_ELBOW, mp.solutions.pose.PoseLandmark.RIGHT_ELBOW, 
@@ -48,6 +93,7 @@ def start_detection():
                           mp_pose.PoseLandmark.LEFT_ELBOW, mp_pose.PoseLandmark.RIGHT_ELBOW,
                           mp_pose.PoseLandmark.LEFT_WRIST, mp_pose.PoseLandmark.RIGHT_WRIST]
     shaking_head_keypoints = [mp.solutions.pose.PoseLandmark.NOSE, mp.solutions.pose.PoseLandmark.LEFT_EAR, mp.solutions.pose.PoseLandmark.RIGHT_EAR]
+    balancing_keypoints = [mp.solutions.pose.PoseLandmark.LEFT_HIP, mp.solutions.pose.PoseLandmark.RIGHT_HIP,mp.solutions.pose.PoseLandmark.LEFT_KNEE, mp.solutions.pose.PoseLandmark.RIGHT_KNEE,mp.solutions.pose.PoseLandmark.LEFT_ANKLE, mp.solutions.pose.PoseLandmark.RIGHT_ANKLE]
 
 
     cap = cv2.VideoCapture(0)
@@ -82,23 +128,40 @@ def start_detection():
                 is_stretching = all([keypoints[k].visibility > 0.5 for k in stretch_keypoints])
                 is_raising_arms = all([keypoints[k].visibility > 0.5 for k in raising_arms_keypoints])
                 is_shaking_head = all([keypoints[k].visibility > 0.5 for k in shaking_head_keypoints])
+                is_standing_still = all([keypoints[k].visibility > 0.5 for k in standing_still_keypoints])
+                is_balancing = all([keypoints[k].visibility > 0.5 for k in balancing_keypoints])
 
-                if is_clapping:
+                if is_standing_still:
+                    detected = 'standing still'
+                    cv2.putText(image, 'Standing still', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                elif is_balancing:
+                    detected = 'balancing on one leg'
+                    cv2.putText(image, 'Balancing on One Leg', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                elif is_raising_arms:
+                    detected = 'raising arms'
+                    cv2.putText(image, 'Raising Arms', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                elif is_clapping:
                     detected = 'clapping'
                     cv2.putText(image, 'Clapping', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 elif is_hi:
+                    detected = 'hi'
                     cv2.putText(image, 'Hi', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 elif is_stretching:
+                    detected = 'stretching'
                     cv2.putText(image, 'Stretching', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif is_raising_arms:
-                    cv2.putText(image, 'Raising Arms', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 elif is_pointing:
+                    detected = 'pointing'
                     cv2.putText(image, 'Pointing', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 elif is_sleeping:
+                    detected = 'sleeping'
                     cv2.putText(image, 'Sleeping', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 elif is_shaking_head:
+                    detected = 'shaking head'
                     cv2.putText(image, 'Shaking Head', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
+                if(speech == category and detected == category):
+                        score = score + 1
+                cv2.putText(image, f'You say : {speech}', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(image, f'Score : {score}', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.imshow('Pose Detection', image)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -116,7 +179,7 @@ def start_video_capture():
 
     cv2.namedWindow("Video Capture", cv2.WINDOW_NORMAL)
     cv2.moveWindow("Video Capture", 100, 100)
-    cv2.resizeWindow("Video Capture", 800, 600)
+    cv2.resizeWindow("Video Capture", 1000, 600)
 
     while True:
         ret, frame = cap.read()
@@ -136,6 +199,9 @@ root.geometry("400x400")
 root.configure(bg="#0D6E9B") 
 label = tk.Label(root, text="Choose a program to start:", fg="#FFFFFF", bg="#0D6E9B", font=("Arial", 24, "bold"))
 label.pack(pady=20)
+detection_button = tk.Button(root, text="Activity", command=create_activity_gui, fg="#FFFFFF", bg="#000000", font=("Arial", 18, "bold"), padx=30, pady=15)
+detection_button.pack(pady=20)
+
 detection_button = tk.Button(root, text="Detection", command=start_detection, fg="#FFFFFF", bg="#000000", font=("Arial", 18, "bold"), padx=30, pady=15)
 detection_button.pack(pady=20)
 
